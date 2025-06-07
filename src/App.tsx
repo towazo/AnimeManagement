205c0e4d357035fd92911d7ccea75c7cf6c38685
towazo <towa.shidori@gmail.com>
2025-06-07 17:20:48 +0900

    fix: BrowserRouterをHashRouterに変更してリロード時の404エラーを解決

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { AnimeProvider } from './context/AnimeContext';
 import { CustomListProvider } from './context/CustomListContext';
